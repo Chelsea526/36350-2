76b8ff4d7391ee53cbf6c97c6f14efd698250f0a
Chelsea526 <TianqiTang@tangtianqideMBP.wv.cc.cmu.edu>
2017-12-09 12:07:12 -0500

    updated

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,14 +1,15 @@
-Hello message to aliens:
+
+print("Hello message to aliens:
   
-Hi Aliens,
-
-I hope you are having a nice day. This is earth. We are the third planet from Sun. 
-We have water adn atomosphere down here and after billion years of evolution we now
-have a complete ecosystem. I myself is a human being, the only high intelligence creature 
-known so far in earth. We are friendly and nice person who wouldl love to make friend with you.
-Being the only object in the Universe known to habor life is a little lonely and if you got
-this message please reply to it.
-
-Looking forward to hearing back from you,
-
-Chelsea
+      Hi Aliens,
+      
+      I hope you are having a nice day. This is earth. We are the third planet from Sun. 
+      We have water adn atomosphere down here and after billion years of evolution we now
+      have a complete ecosystem. I myself is a human being, the only high intelligence creature 
+      known so far in earth. We are friendly and nice person who wouldl love to make friend with you.
+      Being the only object in the Universe known to habor life is a little lonely and if you got
+      this message please reply to it.
+      
+      Looking forward to hearing back from you,
+      
+      Chelsea")
